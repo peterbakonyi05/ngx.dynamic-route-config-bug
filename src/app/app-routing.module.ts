becdf12f7fbe5ea7eb977519afb3207256a3d1ab
BETSSON\peba02 <peter.bakonyi@betssongroup.com>
2017-11-07 13:34:44 +0100

    add example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+const sharedConfig = {
+  data: {
+    trackingArea: 'some-feature',
+    product: 'some-feature'
+  }
+};
+
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: '', pathMatch: 'full', component: HomeComponent, ...sharedConfig },
+  { path: '**', component: NotFoundComponent, ...sharedConfig }
 ];
 
 @NgModule({
